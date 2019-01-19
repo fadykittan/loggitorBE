0d08f29a5cb6375b0abaec0d99d7eccaea39a226
Fady Kittan <fady.kittan@gmail.com>
2019-01-19 02:29:32 +0200

    first commit

---
+++ src/main/java/com/loggitorBE/loggitorBE/LoggitorBeApplication.java
@@ -0,0 +1,14 @@
+package com.loggitorBE.loggitorBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggitorBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoggitorBeApplication.class, args);
+	}
+
+}
+
