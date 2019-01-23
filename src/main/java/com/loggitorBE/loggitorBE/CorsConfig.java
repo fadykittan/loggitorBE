7e1330b34f63db35dcda7b9e05f41372835ade25
Fady Kittan <fady.kittan@gmail.com>
2019-01-23 20:00:15 +0200

    adding CorsConfig

---
+++ src/main/java/com/loggitorBE/loggitorBE/CorsConfig.java
@@ -0,0 +1,28 @@
+package com.loggitorBE.loggitorBE;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+  @Bean
+  public CorsFilter corsFilter() {
+      CorsConfiguration config = new CorsConfiguration();
+      config.addAllowedOrigin("*");
+      config.addAllowedHeader("*");
+      config.setAllowedMethods(Arrays.asList(
+    		  new String[] {"OPTIONS", "GET", "POST", "PUT", "DELETE"}));
+
+      UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+      source.registerCorsConfiguration("/**", config);
+
+      return new CorsFilter(source);
+  }
+}
+
+
