3abf0e54b9523d77f8787489aed9037c0202f1c9
Fady Kittan <fady.kittan@gmail.com>
2019-02-14 17:45:26 +0200

    fixing delete and add API

--- src/main/java/com/loggitorBE/loggitorBE/domain/EventInstanceRepo.java
+++ src/main/java/com/loggitorBE/loggitorBE/domain/EventInstanceRepo.java
@@ -17,6 +17,12 @@ public interface EventInstanceRepo extends CrudRepository<EventInstance, Long>{
 	ArrayList<EventInstanceOnDate> getEventInsTable(@Param("date") Date date,
 			@Param("limit") int limit,
 			@Param("offset") int offset);
+	
+	
+	
+	
+	@Query(value ="SELECT * FROM event_instance WHERE occurred_event = ?1",nativeQuery = true)
+	ArrayList<EventInstance> getEveInsByDefinedEveId(@Param("DefinedEventId") long date);
 
 	
 }	
